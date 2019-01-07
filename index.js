4bdffb1dff01c76ce0388134679707c45b7665b5
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2019-01-07 18:16:28 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const request = require('request');
+var prefix = "*";
+var TobyVersion = "1.0.0";
+
+bot.on('ready', () => {
+	bot.user.setActivity(bot.guilds.membercount + ' fanboys', { type: 'WATCHING' });
+	
+});
+
+bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+
+	let msg = message.content.toUpperCase();
+
+
+	if (msg === "tobyboy") {
+	const embedtoby = new RichEmbed()
+		.attachFile('https://twitter.com/i/status/1073525646326849536')
+		.setFooter("TobyBoy" + TobyVersion)
+		.setThumbnail(bot.user.AvatarURL)
+
+	message.channel.sendEmbed(embedtoby);
+	
+	}
+});
