29f9bc0466a4bc8446f48c5a99ab2df644d27a10
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2019-01-07 23:11:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if(message.author.bot) return;
 	
 let msg = message.content.toUpperCase();
 	
-if (msg = "tobyboy") {
+if(msg = "tobyboy") {
 	const embedtoby = new Discord.RichEmbed()
 		.setAuthor(message.author.name, message.author.avatarURL)
 		.setFooter("TobyBoy " + TobyVersion, bot.user.avatarURL)
@@ -36,11 +36,25 @@ if (msg = "tobyboy") {
 	message.channel.sendEmbed(embedtoby);
 	
 }
-if (msg.startsWith(prefix + "contact")) {
+if(msg.startsWith(prefix + "contact")) {
 	const furbaka = users.get("328514801124900866")
         let args = message.content.split(" ").slice(1);
 	let thingToEcho = args.join(" ");
 	bot.furbaka.sendMessage('Nouveau message : ${thingToEcho}');
+	
+}
+if(msg.startsWith(prefix + "prefix")) {
+let args = message.content.split(" ").slice(1);
+let thingToEcho = args.join(" ");
+	if(thingToEcho = ""){
+const embedprefixnull = new Discord.RichEmbed()
+	.setAuthor(message.author.name, message.author.avatarURL)
+	.setFooter("TobyBoy " + TobyVersion, bot.user.avatarURL)
+	.setColor("953da0")
+	.setTitle("Préfix")
+	.setDescription('le préfix est défini en tant que : **${prefix}**')
+	}
+
 }
 
 });
